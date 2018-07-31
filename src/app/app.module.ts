681c5522fff9b086bae0c49847ed851845cefe0e
blisstonkiruba <blisstonkirubha@gmail.com>
2018-07-31 16:04:45 +0530

    landing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material';
+import {MatInputModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatOptionModule
+                      } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
@@ -10,7 +21,14 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
     LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+        MatOptionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
